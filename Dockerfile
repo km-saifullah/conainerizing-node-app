678de755946eec95559683b3181a7578c6729f98
km-saifullah <kmsaifullah16@gmail.com>
2024-09-18 21:33:05 +0600

    docker file and dockerignore files ready to create docker image

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:22-alpine
+
+WORKDIR /app
+
+COPY package*.json /app
+
+RUN npm install
+
+COPY . /app
+
+EXPOSE 8000
+
+CMD [ "npm", "start" ]
--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import app from "./app.js";
 
 // port
 const PORT = process.env.PORT || 8000;
-const hostname = "127.0.0.10";
+const hostname = "0.0.0.0";
 
 app.listen(PORT, hostname, () => console.log("Server is running"));
